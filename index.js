c587eac38e221764346de0d6e425a81ab210f48a
Rotimi Babalola <rolexjames007@gmail.com>
2016-10-25 10:09:01 +0100

    Made adding numbers more robust

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var program = require('commander')
 var twilio = require('twilio')
 var readline = require('readline')
 
+
+var db = new sqlite3.Database('contacts_db.sqlite')
 //first goal create a database to store the contacts
 program
   .command('add <name> <phone_number>')
@@ -11,18 +13,20 @@ program
   .option('-p, --phone_number', 'Phone number of contact')
   .description('Add a new contact')
   .action(function(name, phone_number, command) {
-    var db = new sqlite3.Database('contacts_db.sqlite')
-
+    //create the table if it does not exist
     db.serialize(function() {
-    	db.run("CREATE TABLE IF NOT EXISTS contacts (contact_name TEXT, contact_number TEXT)");
+    	db.run("CREATE TABLE IF NOT EXISTS contacts (contact_id INTEGER PRIMARY KEY AUTOINCREMENT, contact_name TEXT, contact_number TEXT)");
     });
 
     var stmt = db.prepare("INSERT INTO contacts (contact_name, contact_number) VALUES (?, ?)");
+    //replace the first 0 in a phone number with +234
+    phone_number = phone_number.replace(phone_number.charAt(0), '+234')
+    //add the name and phone number to the database
     stmt.run(name, phone_number)
     stmt.finalize();
     //log the data
-    db.each("SELECT contact_name, contact_number FROM contacts", function(err, row){
-    	console.log(row.contact_name + ":" + row.contact_number)
+    db.each("SELECT contact_id, contact_name, contact_number FROM contacts", function(err, row){
+    	console.log(row.contact_id + ":" + row.contact_name + ":" + row.contact_number)
     });
 
   });
